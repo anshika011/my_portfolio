b45b5f827d24bc3f6d4b487e7e5be3d973e64ed2
ANSHIKA <83632858+anshika011@users.noreply.github.com>
2023-09-02 21:07:36 +0530

    added the js folder
    javascript files for the website

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+/**===================typing animation=================== */
+var typed = new Typed(".typing",{
+    strings:["A Student","An Aspiring Sofware Engineer","A Sports Enthusiast","A Tech Geek"],
+    typespeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
